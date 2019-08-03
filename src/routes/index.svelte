6a18fb3c7ce4bc6d5de1656a951e0e3b4ff7ccb3
Yasar Kücükkaya <yasar@yk-code.de>
2019-08-03 21:08:33 +0200

    Added pages per side

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -24,6 +24,7 @@
   let orientation = "";
   let format = "";
   let customCss = "";
+  let pagesPerSide = "";
   $: isLoaded = false;
 
   $: {
@@ -32,6 +33,7 @@
   }
 
   $: computed_url = "https://percollate-api.herokuapp.com/pdf?url=" + url
+    + `&pagesperside=${pagesPerSide}`
     + "&" + "css="
     + encodeURIComponent(`@page { size: ${format} ${orientation} }`)
     + encodeURIComponent(customCss);
@@ -92,6 +94,21 @@
           </div>
         </div>
         <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+            PAGES PER SIDE
+          </label>
+          <div class="relative">
+            <select bind:value="{pagesPerSide}" class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+              <option value="1" selected>1</option>
+              <option value="2">2</option>
+              <option value="4">4</option>
+            </select>
+            <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+            </div>
+          </div>
+        </div>
+        <div class="w-full md:w-1/3 px-3 mt-6 mb-6 md:mb-0">
           <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
             CUSTOM CSS
           </label>
