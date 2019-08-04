266e5a20f3b52ed7e39ce3b930c518c97529ad36
Yasar Kücükkaya <yasar@yk-code.de>
2019-08-04 22:10:57 +0200

    Fix typing

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -58,7 +58,7 @@
       <h1 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-2 text-3xl md:text-4xl">Percollate API</h1>
     </div>
     <p class="py-6">
-      👋 Welcome to the <a class="text-teal-500 no-underline hover:underline" href="https://github.com/yashha/percollate-api">Percollate API</a> a public API to transform website content to pdf using the percollate package.
+      👋 Welcome to the <a class="text-teal-500 no-underline hover:underline" href="https://github.com/yashha/percollate-api">Percollate API</a> a public API to transform website content to PDF using the percollate package.
     </p>
 
     <form class="w-full" on:submit="{onSubmit}">
