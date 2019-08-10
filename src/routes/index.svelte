420d477ff154ae4201ad1a9a7c0489a921fb2de8
Yasar Kücükkaya <yasar@yk-code.de>
2019-08-10 21:20:19 +0200

    Removed darkreader props

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -92,12 +92,7 @@
               stroke-width="2"
               fill="none"
               stroke-linecap="round"
-              stroke-linejoin="round"
-              class="css-i6dzq1"
-              style="--darkreader-inline-fill:none;
-              --darkreader-inline-stroke:currentColor;"
-              data-darkreader-inline-fill=""
-              data-darkreader-inline-stroke="">
+              stroke-linejoin="round">
               <circle cx="12" cy="12" r="10" />
               <line x1="15" y1="9" x2="9" y2="15" />
               <line x1="9" y1="9" x2="15" y2="15" />
