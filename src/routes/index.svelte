94e5aeea7dba243a745be0cb347146befd1e4e88
Yasar Kücükkaya <yasar@yk-code.de>
2019-08-13 22:47:03 +0200

    Fixed label

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -99,35 +99,39 @@
     </p>
 
     <form class="w-full" on:submit={onSubmit}>
-      <label class="relative block" for="url">
-        <input
-          class="paclient__search bg-gray-200 focus:outline-0
-          focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block
-          w-full appearance-none leading-normal pr-10"
-          type="url"
-          name="url"
-          id="url"
-          placeholder="Enter url"
-          required
-          bind:value={url} />
-        {#if url}
-          <div class="absolute right-0 top-0 mt-3 mr-3" on:click={clearInput}>
-            <svg
-              viewBox="0 0 24 24"
-              width="24"
-              height="24"
-              stroke="currentColor"
-              stroke-width="2"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round">
-              <circle cx="12" cy="12" r="10" />
-              <line x1="15" y1="9" x2="9" y2="15" />
-              <line x1="9" y1="9" x2="15" y2="15" />
-            </svg>
-          </div>
-        {/if}
+      <label
+        class="block uppercase tracking-wide text-gray-700 text-xs font-bold
+        mb-2"
+        for="url">
+        URL
       </label>
+      <input
+        class="paclient__search bg-gray-200 focus:outline-0 focus:shadow-outline
+        border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none
+        leading-normal pr-10"
+        type="url"
+        name="url"
+        id="url"
+        placeholder="Enter url"
+        required
+        bind:value={url} />
+      {#if url}
+        <div class="absolute right-0 top-0 mt-3 mr-3" on:click={clearInput}>
+          <svg
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            stroke="currentColor"
+            stroke-width="2"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round">
+            <circle cx="12" cy="12" r="10" />
+            <line x1="15" y1="9" x2="9" y2="15" />
+            <line x1="9" y1="9" x2="15" y2="15" />
+          </svg>
+        </div>
+      {/if}
 
       <br />
 
