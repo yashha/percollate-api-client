9c62a80e4d3bd4e340170b520daf9cd1b4cdc4f4
Yasar Kücükkaya <yasar@yk-code.de>
2019-08-20 01:00:59 +0200

    Removed configurable orientation

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,7 +27,7 @@
         '&' +
         'css=' +
         encodeURIComponent(`html { font-size: ${fontSize}pt }`) +
-        encodeURIComponent(`@page { size: ${format} ${orientation} }`) +
+        encodeURIComponent(`@page { size: ${format} portrait }`) +
         encodeURIComponent(customCss);
     } catch (e) {
       computed_url = '';
@@ -138,7 +138,7 @@
       <br />
 
       <div class="flex flex-wrap -mx-3 mb-2">
-        <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0">
+        <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold
             mb-2"
@@ -172,41 +172,7 @@
             </div>
           </div>
         </div>
-        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-          <label
-            class="block uppercase tracking-wide text-gray-700 text-xs font-bold
-            mb-2"
-            for="orientation">
-            ORIENTATION
-          </label>
-          <div class="relative">
-            <select
-              bind:value={orientation}
-              class="block appearance-none w-full bg-gray-200 border
-              border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight
-              focus:outline-none focus:bg-white focus:border-gray-500"
-              id="orientation">
-              <option value="portrait" selected>portrait</option>
-              <option value="landscape">landscape</option>
-            </select>
-            <div
-              class="pointer-events-none absolute inset-y-0 right-0 flex
-              items-center px-2 text-gray-700">
-              <svg
-                class="h-4 w-4"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-                fill="none"
-                stroke-linecap="round"
-                stroke-linejoin="round">
-                <polyline points="6 9 12 15 18 9" />
-              </svg>
-            </div>
-          </div>
-        </div>
-        <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+        <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold
             mb-2"
@@ -241,7 +207,7 @@
             </div>
           </div>
         </div>
-        <div class="w-full md:w-1/6 px-3 mb-6 md:mb-0">
+        <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
           <label
             class="block uppercase tracking-wide text-gray-700 text-xs font-bold
             mb-2"
