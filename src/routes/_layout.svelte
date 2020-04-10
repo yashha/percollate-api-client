fc5b1542ef939bbbcfc9caacb8cb2b560ccee811
Y K <yasarkkk@protonmail.com>
2020-04-10 23:45:57 +0200

    Move head into layout

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,6 +6,13 @@
 
 </style>
 
+<svelte:head>
+  <title>Read the web!</title>
+  <meta
+    name="description"
+    content="Make the web more readable and printable by generating clean pdf." />
+</svelte:head>
+
 <main>
   <slot />
 </main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -139,12 +139,6 @@
   }
 </style>
 
-<svelte:head>
-  <title>Read the web!</title>
-  <meta
-    name="description"
-    content="Make the web more readable and printable by generating clean pdf." />
-</svelte:head>
 <div class="container w-full md:max-w-3xl mx-auto pt-20">
 
   <div
