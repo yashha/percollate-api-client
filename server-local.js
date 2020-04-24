f90e2ba3e7619c4be4966a3be050c56078c9f0d8
Y K <yasarkkk@protonmail.com>
2020-04-25 00:32:00 +0200

    renamed function

--- server-local.js
+++ server-local.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const app = require('./functions');
+const app = require('./functions/server');
 
 app.listen(3000, () =>
   console.log('Local app listening on http://localhost:3000!')
