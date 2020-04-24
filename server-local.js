060a8c1e9529d695c1df23105c6971ed87bbc440
Y K <yasarkkk@protonmail.com>
2020-04-24 23:50:15 +0200

    changed function name

--- server-local.js
+++ server-local.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const app = require('./functions/server');
+const app = require('./functions');
 
 app.listen(3000, () =>
   console.log('Local app listening on http://localhost:3000!')
