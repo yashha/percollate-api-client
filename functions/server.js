3a25a7658927ea0ec5d86a82c4b3e7737e483fad
Y K <yasarkkk@protonmail.com>
2020-06-05 19:46:54 +0200

    Switched to vercel deploy

--- functions/server.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-const serverless = require('serverless-http');
-const app = require('../__sapper__/build/server/server');
-
-module.exports = app;
-module.exports.handler = serverless(app);
